b2e883a5d57a9f16226812036f8eb43b2e73e459
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-03-03 23:50:41 +0530

    Server app

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import colors from 'colors';
+import cors from 'cors';
+import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+    res.send('Server running')
+})
+
+app.use('/api/products', productRoutes);
+
+app.use(notFound)
+app.use(errorHandler)
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, 
+    console.log(`Server running on ${process.env.NODE_ENV} mode port ${process.env.PORT}`.bgYellow.black.underline)
+);
