e515a9ea58a97dce088c2082022756ff23daed62
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-03-03 23:57:47 +0530

    React index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,11 +4,13 @@ import App from './App';
 import './index.css';
 import './bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
