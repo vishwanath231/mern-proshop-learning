94b14c7b3e6bdba7333fc251d9941b09673f85e1
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-03-03 23:56:38 +0530

    Product screen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/Rating';
-import products from '../products';
+import axios from 'axios';
+
 
 
 const ProductScreen = () => {
 
     const { id } = useParams();
-    const product = products.find((p) => p._id === id);
+
+    const [product, setProduct] = useState([]);
+
+    useEffect(() => {
+        
+        axios.get(`http://localhost:5000/api/products/${id}`)
+        .then(res => {
+            setProduct(res.data)
+        })
+        
+    }, [id]);
+
 
     return (
         <>
