a732e9a568de71b4d30a1e509c8a887704fb7e8a
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-03-05 00:42:10 +0530

    React index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
 import './bootstrap.min.css';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './redux/store';
