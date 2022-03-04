3b1e8a8a2901105c3450b952f567ca99cea9dfa2
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-03-05 00:48:24 +0530

    Product reducers

--- frontend/src/redux/reducers/productReducers.js
+++ frontend/src/redux/reducers/productReducers.js
@@ -1,7 +1,10 @@
 import {
     PRODUCT_LIST_REQUEST,
     PRODUCT_LIST_SUCCESS,
-    PRODUCT_LIST_FAIL
+    PRODUCT_LIST_FAIL,
+    PRODUCT_DETAILS_REQUEST,
+    PRODUCT_DETAILS_SUCCESS,
+    PRODUCT_DETAILS_FAIL
 } from '../constants/productConstants';
 
 export const productListReducer = (state = { products:[] }, {type, payload}) => {
@@ -20,6 +23,36 @@ export const productListReducer = (state = { products:[] }, {type, payload}) => 
             }
 
         case PRODUCT_LIST_FAIL:
+            return {
+                loading: false,
+                error: payload
+            }
+
+        default:
+            return state;
+    }
+}
+
+
+
+
+
+export const productDetailsReducer = (state = { product:{ reviews:[] } }, {type, payload}) => {
+     
+    switch (type) {
+        case PRODUCT_DETAILS_REQUEST:
+            return {
+                loading: true,
+                ...state
+            }
+
+        case PRODUCT_DETAILS_SUCCESS:
+            return {
+                loading: false,
+                product: payload
+            }
+
+        case PRODUCT_DETAILS_FAIL:
             return {
                 loading: false,
                 error: payload
