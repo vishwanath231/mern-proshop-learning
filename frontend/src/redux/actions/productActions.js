905a97eb356196c9377f703c1167dd0e66671ec3
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-03-05 00:45:24 +0530

    Product actions

--- frontend/src/redux/actions/productActions.js
+++ frontend/src/redux/actions/productActions.js
@@ -1,7 +1,10 @@
 import {
     PRODUCT_LIST_REQUEST,
     PRODUCT_LIST_SUCCESS,
-    PRODUCT_LIST_FAIL
+    PRODUCT_LIST_FAIL,
+    PRODUCT_DETAILS_REQUEST,
+    PRODUCT_DETAILS_SUCCESS,
+    PRODUCT_DETAILS_FAIL
 } from '../constants/productConstants';
 import axios from 'axios';
 
@@ -24,6 +27,32 @@ export const listProducts = () => async (dispatch) => {
         
         dispatch({
             type: PRODUCT_LIST_FAIL,
+            payload: err.response && err.response.data.message ? err.response.data.message : err.message
+        })
+    }
+}
+
+
+
+
+export const listProductDetails = (id) => async (dispatch) => {
+
+    try {
+        
+
+        dispatch({ type: PRODUCT_DETAILS_REQUEST });
+
+        const { data } = await axios.get(`http://localhost:5000/api/products/${id}`)
+
+        dispatch({
+            type: PRODUCT_DETAILS_SUCCESS,
+            payload: data
+        })
+
+    } catch (err) {
+        
+        dispatch({
+            type: PRODUCT_DETAILS_FAIL,
             payload: err.response && err.response.data.message ? err.response.data.message : err.message
         })
     }
