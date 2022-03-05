8f4350ec04f99a802dbc2d1f94d77ef2f60ab269
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-03-05 23:50:05 +0530

    Reducers index

--- frontend/src/redux/reducers/index.js
+++ frontend/src/redux/reducers/index.js
@@ -1,9 +1,19 @@
 import { combineReducers } from 'redux';
 import { productListReducer, productDetailsReducer } from './productReducers';
 import { cartReducer } from './cartReducers';
+import { 
+    userLoginReducer, 
+    userRegisterReducer, 
+    userDetailsReducer, 
+    userUpdateProfileReducer 
+} from './userReducers';
 
 export const reducers = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    userLogin: userLoginReducer,
+    userRegister: userRegisterReducer,
+    userDetails: userDetailsReducer,
+    userUpdateProfile: userUpdateProfileReducer
 });
