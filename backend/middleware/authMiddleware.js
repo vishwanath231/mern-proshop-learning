50214f01f6c6910c7de36442333ae86137cf1b9b
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-03-08 20:25:11 +0530

    Auth middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -33,4 +33,16 @@ const protect = asyncHandler(async (req, res, next) => {
 
 })
 
-export default protect;
+
+
+
+const admin = (req, res, next) => {
+    if (req.user && req.user.isAdmin) {
+        next()
+    } else {
+        res.status(401)
+        throw new Error('Not authorized as an admin')
+    }
+  }
+
+export { protect, admin };
