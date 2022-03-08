ad55349339d0987f6ab56eb310278a253547b84b
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-03-08 20:35:58 +0530

    User actions

--- frontend/src/redux/actions/userActions.js
+++ frontend/src/redux/actions/userActions.js
@@ -11,8 +11,14 @@ import {
     USER_UPDATE_PROFILE_REQUEST,
     USER_UPDATE_PROFILE_SUCCESS,
     USER_UPDATE_PROFILE_FAIL,
-    USER_LOGOUT
+    USER_LOGOUT,
+    USER_DETAILS_RESET,
+    USER_LIST_REQUEST,
+    USER_LIST_SUCCESS,
+    USER_LIST_FAIL,
+    USER_LIST_RESET
 } from '../constants/userConstants';
+import { ORDER_LIST_MY_RESET } from '../constants/orderConstants';
 import axios from 'axios';
 
 
@@ -165,12 +171,64 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
 
 
 
+export const listUsers = () => async (dispatch, getState) => {
+
+    try {
+        
+        dispatch({
+            type: USER_LIST_REQUEST
+        })
+
+        const { userLogin: { userInfo } } = getState()
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        const { data } = await axios.get(`http://localhost:5000/api/users`, config)
+
+        dispatch({
+            type: USER_LIST_SUCCESS,
+            payload: data
+        })
+
+        console.log(data);
+
+
+    } catch (err) {
+        
+        dispatch({
+            type: USER_LIST_FAIL,
+            payload: err.response && err.response.data.message ? err.response.data.message : err.message
+        })
+    }
+}
+
+
+
+
+
 
 
 export const logout = () => (dispatch) => {
-    localStorage.removeItem('userInfo');
+    localStorage.removeItem('userInfo')
+    localStorage.removeItem('cartItems')
+    localStorage.removeItem('shippingAddress')
+    localStorage.removeItem('paymentMethod')
    
     dispatch({
         type: USER_LOGOUT
+    })
+    dispatch({
+        type: USER_DETAILS_RESET
+    })
+    dispatch({
+        type: ORDER_LIST_MY_RESET
+    })
+    dispatch({
+        type: USER_LIST_RESET
     })
 }
