b59c7356138054f644adc55a4204842071a2ce58
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-03-08 20:26:53 +0530

    User  model

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -25,6 +25,6 @@ const userSchema = mongoose.Schema({
     timestamps: true
 })
 
-const User = mongoose.model('users', userSchema);
+const User = mongoose.model('User', userSchema);
 
 export default User;
