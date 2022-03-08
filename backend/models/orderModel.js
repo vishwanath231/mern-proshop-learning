460d90a33a9be6ba77cdf7da34a9740e2246b582
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-03-08 20:26:03 +0530

    Order model

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -101,6 +101,6 @@ const orderSchema = mongoose.Schema({
     timestamps: true
 })
 
-const Order = mongoose.model('orders', orderSchema);
+const Order = mongoose.model('Order', orderSchema);
 
 export default Order;
