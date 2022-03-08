90f185c0d7ce94997e9efeb9dc9897683543d385
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-03-08 22:08:35 +0530

    User routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,12 @@
 import { Router } from "express";
-import { register, login, getUserProfile, updateUserProfile, getUsers } from "../controllers/userController.js";
+import { 
+    register, 
+    login, 
+    getUserProfile, 
+    updateUserProfile, 
+    getUsers,
+    deleteUser 
+} from "../controllers/userController.js";
 const router = Router();
 import { protect, admin } from '../middleware/authMiddleware.js';
 
@@ -7,6 +14,7 @@ import { protect, admin } from '../middleware/authMiddleware.js';
 router.route('/').post(register).get(protect, admin, getUsers)
 router.route('/login').post(login)
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
+router.route('/:id').delete(protect, admin, deleteUser)
 
 
 export default router;
