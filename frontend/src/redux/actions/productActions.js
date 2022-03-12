cdc81b6481b9e636f103cd3d4c7491eb4537b0d7
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-03-12 22:04:29 +0530

    Product actions

--- frontend/src/redux/actions/productActions.js
+++ frontend/src/redux/actions/productActions.js
@@ -4,7 +4,16 @@ import {
     PRODUCT_LIST_FAIL,
     PRODUCT_DETAILS_REQUEST,
     PRODUCT_DETAILS_SUCCESS,
-    PRODUCT_DETAILS_FAIL
+    PRODUCT_DETAILS_FAIL,
+    PRODUCT_DELETE_REQUEST,
+    PRODUCT_DELETE_SUCCESS,
+    PRODUCT_DELETE_FAIL,
+    PRODUCT_CREATE_REQUEST,
+    PRODUCT_CREATE_SUCCESS,
+    PRODUCT_CREATE_FAIL,
+    PRODUCT_UPDATE_REQUEST,
+    PRODUCT_UPDATE_SUCCESS,
+    PRODUCT_UPDATE_FAIL
 } from '../constants/productConstants';
 import axios from 'axios';
 
@@ -38,7 +47,6 @@ export const listProducts = () => async (dispatch) => {
 export const listProductDetails = (id) => async (dispatch) => {
 
     try {
-        
 
         dispatch({ type: PRODUCT_DETAILS_REQUEST });
 
@@ -53,6 +61,117 @@ export const listProductDetails = (id) => async (dispatch) => {
         
         dispatch({
             type: PRODUCT_DETAILS_FAIL,
+            payload: err.response && err.response.data.message ? err.response.data.message : err.message
+        })
+    }
+}
+
+
+
+
+
+export const deleteProduct = (id) => async (dispatch, getState) => {
+
+    try {
+        
+        dispatch({
+            type: PRODUCT_DELETE_REQUEST
+        })
+
+        const { userLogin: { userInfo } } = getState()
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        await axios.delete(`http://localhost:5000/api/products/${id}`, config)
+
+        dispatch({
+            type: PRODUCT_DELETE_SUCCESS,
+        })
+
+    } catch (err) {
+        
+        dispatch({
+            type: PRODUCT_DELETE_FAIL,
+            payload: err.response && err.response.data.message ? err.response.data.message : err.message
+        })
+    }
+}
+
+
+
+
+
+export const createProduct = () => async (dispatch, getState) => {
+
+    try {
+        
+        dispatch({
+            type: PRODUCT_CREATE_REQUEST
+        })
+
+        const { userLogin: { userInfo } } = getState()
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        const { data } = await axios.post(`http://localhost:5000/api/products`, {}, config)
+
+        dispatch({
+            type: PRODUCT_CREATE_SUCCESS,
+            payload: data
+        })
+
+    } catch (err) {
+        
+        dispatch({
+            type: PRODUCT_CREATE_FAIL,
+            payload: err.response && err.response.data.message ? err.response.data.message : err.message
+        })
+    }
+}
+
+
+
+
+
+
+export const updateProduct = (product) => async (dispatch, getState) => {
+
+    try {
+        
+        dispatch({
+            type: PRODUCT_UPDATE_REQUEST
+        })
+
+        const { userLogin: { userInfo } } = getState()
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        const { data } = await axios.put(`http://localhost:5000/api/products/${product._id}`, product, config)
+
+        dispatch({
+            type: PRODUCT_UPDATE_SUCCESS,
+            payload: data
+        })
+
+    } catch (err) {
+        
+        dispatch({
+            type: PRODUCT_UPDATE_FAIL,
             payload: err.response && err.response.data.message ? err.response.data.message : err.message
         })
     }
