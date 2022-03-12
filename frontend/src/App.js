640378a759ce7e731add7f024d5747d6207f71c5
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-03-12 22:10:07 +0530

    React app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,9 @@ import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
 import UserListScreen from './screens/UserListScreen';
+import UserEditScreen from './screens/UserEditScreen';
+import ProductListScreen from './screens/ProductListScreen';
+import ProductEditScreen from './screens/ProductEditScreen';
 
 
 const App = () => {
@@ -36,6 +39,9 @@ const App = () => {
                         <Route path='/placeorder'  element={ <PlaceOrderScreen /> } />
                         <Route path='/order/:id'  element={ <OrderScreen /> } />
                         <Route path='/admin/userlist'  element={ <UserListScreen /> } />
+                        <Route path='/admin/user/:id/edit'  element={ <UserEditScreen /> } />
+                        <Route path='/admin/productlist'  element={ <ProductListScreen /> } />
+                        <Route path='/admin/product/:id/edit'  element={ <ProductEditScreen /> } />
                     </Routes>
                 </Container> 
             </main>
