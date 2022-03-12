07bbc1de902f506d9d5bef8d9da77ddba71a8a2d
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-03-12 22:04:53 +0530

    User actions

--- frontend/src/redux/actions/userActions.js
+++ frontend/src/redux/actions/userActions.js
@@ -8,18 +8,21 @@ import {
     USER_DETAILS_REQUEST,
     USER_DETAILS_SUCCESS,
     USER_DETAILS_FAIL,
+    USER_DETAILS_RESET,
     USER_UPDATE_PROFILE_REQUEST,
     USER_UPDATE_PROFILE_SUCCESS,
     USER_UPDATE_PROFILE_FAIL,
     USER_LOGOUT,
-    USER_DETAILS_RESET,
     USER_LIST_REQUEST,
     USER_LIST_SUCCESS,
     USER_LIST_FAIL,
     USER_LIST_RESET,
     USER_DELETE_REQUEST,
     USER_DELETE_SUCCESS,
-    USER_DELETE_FAIL
+    USER_DELETE_FAIL,
+    USER_UPDATE_REQUEST,
+    USER_UPDATE_SUCCESS,
+    USER_UPDATE_FAIL
 } from '../constants/userConstants';
 import { ORDER_LIST_MY_RESET } from '../constants/orderConstants';
 import axios from 'axios';
@@ -246,6 +249,52 @@ export const deleteUser = (id) => async (dispatch, getState) => {
 
         dispatch({
             type: USER_DELETE_FAIL,
+            payload: message,
+        })
+    }
+}
+
+
+
+
+
+
+export const updateUser = (user) => async (dispatch, getState) => {
+
+    try {
+        
+        dispatch({
+            type: USER_UPDATE_REQUEST
+        })
+
+        const { userLogin: { userInfo } } = getState()
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+    const { data } = await axios.put(`http://localhost:5000/api/users/${user._id}`, user, config)
+
+        dispatch({ type: USER_UPDATE_SUCCESS })
+
+        dispatch({
+            type: USER_DELETE_SUCCESS,
+            payload: data
+        })
+
+        dispatch({ type: USER_DETAILS_RESET })
+
+    } catch (error) {
+        
+        const message = error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message
+
+        dispatch({
+            type: USER_UPDATE_FAIL,
             payload: message,
         })
     }
