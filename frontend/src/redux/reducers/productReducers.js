53c7e5225c45e1cf9ebe4b0994e6ec011856c46b
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-03-12 22:06:15 +0530

    Product reducers

--- frontend/src/redux/reducers/productReducers.js
+++ frontend/src/redux/reducers/productReducers.js
@@ -4,7 +4,18 @@ import {
     PRODUCT_LIST_FAIL,
     PRODUCT_DETAILS_REQUEST,
     PRODUCT_DETAILS_SUCCESS,
-    PRODUCT_DETAILS_FAIL
+    PRODUCT_DETAILS_FAIL,
+    PRODUCT_DELETE_REQUEST,
+    PRODUCT_DELETE_SUCCESS,
+    PRODUCT_DELETE_FAIL,
+    PRODUCT_CREATE_REQUEST,
+    PRODUCT_CREATE_SUCCESS,
+    PRODUCT_CREATE_FAIL,
+    PRODUCT_CREATE_RESET,
+    PRODUCT_UPDATE_REQUEST,
+    PRODUCT_UPDATE_SUCCESS,
+    PRODUCT_UPDATE_FAIL,
+    PRODUCT_UPDATE_RESET
 } from '../constants/productConstants';
 
 export const productListReducer = (state = { products:[] }, {type, payload}) => {
@@ -58,6 +69,105 @@ export const productDetailsReducer = (state = { product:{ reviews:[] } }, {type,
                 error: payload
             }
 
+        default:
+            return state;
+    }
+}
+
+
+
+
+
+
+export const productDeleteReducer = (state = { }, {type, payload}) => {
+     
+    switch (type) {
+        case PRODUCT_DELETE_REQUEST:
+            return {
+                loading: true,
+                ...state
+            }
+
+        case PRODUCT_DELETE_SUCCESS:
+            return {
+                loading: false,
+                success: true
+            }
+
+        case PRODUCT_DELETE_FAIL:
+            return {
+                loading: false,
+                error: payload
+            }
+
+        default:
+            return state;
+    }
+}
+
+
+
+
+
+export const productCreateReducer = (state = {}, {type, payload}) => {
+     
+    switch (type) {
+        case PRODUCT_CREATE_REQUEST:
+            return {
+                loading: true,
+                ...state
+            }
+
+        case PRODUCT_CREATE_SUCCESS:
+            return {
+                loading: false,
+                success: true,
+                product: payload
+            }
+
+        case PRODUCT_CREATE_FAIL:
+            return {
+                loading: false,
+                error: payload
+            }
+        case PRODUCT_CREATE_RESET:
+            return {}
+            
+        default:
+            return state;
+    }
+}
+
+
+
+
+
+export const productUpdateReducer = (state = { product: {} }, {type, payload}) => {
+     
+    switch (type) {
+        case PRODUCT_UPDATE_REQUEST:
+            return {
+                loading: true,
+                ...state
+            }
+
+        case PRODUCT_UPDATE_SUCCESS:
+            return {
+                loading: false,
+                success: true,
+                product: payload
+            }
+
+        case PRODUCT_UPDATE_FAIL:
+            return {
+                loading: false,
+                error: payload
+            }
+        case PRODUCT_UPDATE_RESET:
+            return {
+                product: {}
+            }
+            
         default:
             return state;
     }
