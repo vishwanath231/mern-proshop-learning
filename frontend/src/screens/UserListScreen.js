d3a30d8c5ab077a874fc2329deefb066732bc434
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-03-12 22:09:21 +0530

    User list screen

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -15,7 +15,6 @@ const UserListScreen = () => {
     const userList = useSelector((state) => state.userList)
     const { loading, error, users } = userList
 
-    console.log(users);
     const userLogin = useSelector((state) => state.userLogin)
     const { userInfo } = userLogin
 
@@ -62,28 +61,28 @@ const UserListScreen = () => {
                                 <td>{user._id}</td>
                                 <td>{user.name}</td>
                                 <td>
-                                <a href={`mailto:${user.email}`}>{user.email}</a>
+                                    <a href={`mailto:${user.email}`}>{user.email}</a>
                                 </td>
                                 <td>
-                                {user.isAdmin ? (
-                                    <i className='fas fa-check' style={{ color: 'green' }}></i>
-                                ) : (
-                                    <i className='fas fa-times' style={{ color: 'red' }}></i>
-                                )}
+                                    {user.isAdmin ? (
+                                        <i className='fas fa-check' style={{ color: 'green' }}></i>
+                                    ) : (
+                                        <i className='fas fa-times' style={{ color: 'red' }}></i>
+                                    )}
                                 </td>
                                 <td>
-                                <LinkContainer to={`/admin/user/${user._id}/edit`}>
-                                    <Button variant='light' className='btn-sm'>
-                                    <i className='fas fa-edit'></i>
+                                    <LinkContainer to={`/admin/user/${user._id}/edit`}>
+                                        <Button variant='light' className='btn-sm'>
+                                            <i className='fas fa-edit'></i>
+                                        </Button>
+                                    </LinkContainer>
+                                    <Button
+                                        variant='danger'
+                                        className='btn-sm'
+                                        onClick={() => deleteHandler(user._id)}
+                                    >
+                                        <i className='fas fa-trash'></i>
                                     </Button>
-                                </LinkContainer>
-                                <Button
-                                    variant='danger'
-                                    className='btn-sm'
-                                    onClick={() => deleteHandler(user._id)}
-                                >
-                                    <i className='fas fa-trash'></i>
-                                </Button>
                                 </td>
                             </tr>
                         ))}
@@ -94,4 +93,4 @@ const UserListScreen = () => {
     )
 }
 
-export default UserListScreen
+export default UserListScreen;
