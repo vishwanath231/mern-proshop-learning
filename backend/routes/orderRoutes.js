0d7c498016c6acda7646a3faf83eb65ad359aab9
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-03-14 14:34:24 +0530

    Order routes

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,13 +1,21 @@
 import { Router } from "express";
-import { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders } from "../controllers/orderController.js";
 const router = Router();
-import { protect }from '../middleware/authMiddleware.js';
+import { protect, admin }from '../middleware/authMiddleware.js';
+import { 
+    addOrderItems, 
+    getOrderById, 
+    updateOrderToPaid, 
+    getMyOrders,
+    getOrders,
+    updateOrderToDelivered 
+} from "../controllers/orderController.js";
 
 
-router.route('/').post(protect, addOrderItems)
+router.route('/').post(protect, addOrderItems).get(protect, admin, getOrders)
 router.route('/myorders').get(protect, getMyOrders)
 router.route('/:id').get(protect, getOrderById)
 router.route('/:id/pay').put(protect, updateOrderToPaid)
+router.route('/:id/deliver').put(protect, admin, updateOrderToDelivered)
 
 
 export default router;
