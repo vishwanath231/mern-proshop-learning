003483d5b032d589a261e2fec7e298b82e65f16c
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-03-14 14:38:05 +0530

    Order actions

--- frontend/src/redux/actions/orderActions.js
+++ frontend/src/redux/actions/orderActions.js
@@ -10,9 +10,16 @@ import {
     ORDER_PAY_FAIL,
     ORDER_LIST_MY_REQUEST,
     ORDER_LIST_MY_SUCCESS,
-    ORDER_LIST_MY_FAIL
+    ORDER_LIST_MY_FAIL,
+    ORDER_LIST_REQUEST,
+    ORDER_LIST_SUCCESS,
+    ORDER_LIST_FAIL,
+    ORDER_DELIVER_FAIL,
+    ORDER_DELIVER_SUCCESS,
+    ORDER_DELIVER_REQUEST
 } from '../constants/orderConstants';
 import axios from 'axios';
+import { logout } from './userActions';
 
 
 export const createOrder = (order) => async (dispatch, getState) => {
@@ -154,3 +161,83 @@ export const listMyOrders = () => async (dispatch, getState) => {
         })
     }
 }
+
+
+
+
+
+
+export const listOrders = () => async (dispatch, getState) => {
+
+    try {
+        
+        dispatch({
+            type: ORDER_LIST_REQUEST
+        })
+
+        const { userLogin: { userInfo } } = getState()
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        const { data } = await axios.get(`http://localhost:5000/api/orders`, config)
+
+        dispatch({
+            type: ORDER_LIST_SUCCESS,
+            payload: data
+        })
+
+    } catch (err) {
+        
+        dispatch({
+            type: ORDER_LIST_FAIL,
+            payload: err.response && err.response.data.message ? err.response.data.message : err.message
+        })
+    }
+}
+
+
+
+
+export const deliverOrder = (order) => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: ORDER_DELIVER_REQUEST,
+        })
+    
+        const {
+            userLogin: { userInfo },
+        } = getState()
+    
+        const config = {
+            headers: {
+                Authorization: `Bearer ${userInfo.token}`,
+            },
+        }
+  
+        const { data } = await axios.put(`http://localhost:5000/api/orders/${order._id}/deliver`, {}, config)
+    
+        dispatch({
+            type: ORDER_DELIVER_SUCCESS,
+            payload: data,
+        })
+
+    } catch (error) {
+        
+        const message =
+            error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message
+        if (message === 'Not authorized, token failed') {
+            dispatch(logout())
+        }
+        dispatch({
+            type: ORDER_DELIVER_FAIL,
+            payload: message,
+        })
+    }
+  }
