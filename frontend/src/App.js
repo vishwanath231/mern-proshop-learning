9e55adb1b0e13a033f220a41f5a98b4f88305e57
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-03-16 00:06:33 +0530

    React app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,6 +18,7 @@ import UserEditScreen from './screens/UserEditScreen';
 import ProductListScreen from './screens/ProductListScreen';
 import ProductEditScreen from './screens/ProductEditScreen';
 import OrderListScreen from './screens/OrderListScreen';
+import SearchBox from './components/SearchBox';
 
 
 const App = () => {
@@ -44,6 +45,7 @@ const App = () => {
                         <Route path='/admin/productlist'  element={ <ProductListScreen /> } />
                         <Route path='/admin/product/:id/edit'  element={ <ProductEditScreen /> } />
                         <Route path='/admin/orderlist'  element={ <OrderListScreen /> } />
+                        <Route path='/search/:keyword'  element={ <SearchBox /> } />
                     </Routes>
                 </Container> 
             </main>
