e64aba4a39e8d0145ec7277133c5d3cdfc7dbffb
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-03-16 00:00:39 +0530

    Server app

--- backend/server.js
+++ backend/server.js
@@ -9,12 +9,18 @@ import orderRoutes from './routes/orderRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import path from 'path';
+import morgan from 'morgan';
+
 
 dotenv.config();
 
 connectDB();
 
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json());
