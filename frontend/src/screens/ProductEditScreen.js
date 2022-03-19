36be6ec3d4f4c50f006afee2f55433627c348440
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-03-19 20:40:14 +0530

    Product edit screen

--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -74,7 +74,7 @@ const ProductEditScreen = () => {
                 }
             }
 
-            const { data } = await axios.post('http://localhost:5000/api/upload', formData, config)
+            const { data } = await axios.post('/api/upload', formData, config)
 
             setImage(data)
             setUploading(false)
